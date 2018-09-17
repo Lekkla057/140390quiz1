3e602e53134dfb8d61a889ab971a8e8e722c3fa7
unknown <Noppadon kaewkong>
2018-09-17 17:48:28 +0700

    finish3

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
-
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
 
 var app = express();
 
