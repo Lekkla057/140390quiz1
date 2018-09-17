8bdc77a0af2a7dcc120e6340896a20052974a644
unknown <Noppadon kaewkong>
2018-09-17 17:35:42 +0700

    finish2

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ var express = require('express');
 
 
 var app = express();
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'www.db4free.net',
-  user     : 's140390',
-  password : 'abc123**',
-  database : 'db140390'
-});
+
 app.set('view engine','ejs');
 app.get('/', function(req, res) {
     res.render('pages/home');
 });
 
 
-app.get('/students', function(req, res) {
+app.get('/students', function(req, res) {var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
   connection.connect()
 connection.query('SELECT * from students', function (err, rows, fields) {
 
@@ -29,6 +29,13 @@ connection.query('SELECT * from students', function (err, rows, fields) {
 });
 
 app.get('/subjects', function(req, res) {
+  var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
   connection.connect()
 connection.query('SELECT * from subjects', function (err, rows, fields) {
 
